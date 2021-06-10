b9fc22b130a2f645433ae409acf6919d17f2708e
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-10 18:17:41 -0400

    Create guest test for function and instance of

---
+++ src/Guest.js
@@ -0,0 +1,8 @@
+class Guest {
+  constructor() {
+
+  }
+}
+
+
+export default Guest;
---
+++ test/Guest-test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Guest from './src/Guest'
+
+
+describe('Guest', () => {
+
+  beforeEach() => {
+
+  }
+
+  it.skip('should be a function', () => {
+    expect(Guest).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Guest', () => {
+    expect(guest).to.be.an.instanceof(Guest);
+  });
+
+
+})
