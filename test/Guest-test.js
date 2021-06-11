575178f177b1a76e491808be8e733ac058269bb7
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-11 07:47:23 -0400

    Move class files to class folder and add files for apicalls and test data

--- test/Guest-test.js
+++ test/Guest-test.js
@@ -17,5 +17,20 @@ describe('Guest', () => {
     expect(guest).to.be.an.instanceof(Guest);
   });
 
+  it.skip('should have an id', () => {
+    expect(guest.id).to.equal(1);
+  });
+
+  it.skip('should have a name', () => {
+    expect(guest.name).to.equal('Leatha Ullrich');
+  });
+
+  it.skip('should have a way to get all of a travelers bookings', () => {
+    
+  })
+
+  it.skip('should have way to get the total amount spent at the hotel', () => {
+
+  })
 
 })
