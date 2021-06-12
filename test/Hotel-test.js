20650afc814d38d919a804512b6f80cb786231c5
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-12 10:21:33 -0400

    Add tests to check for hotel properties

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -1,13 +1,43 @@
 import chai from 'chai';
 const expect = chai.expect;
 import Hotel from './src/classes/Hotel'
+import Room from './src/classes/Room'
+import Booking from './src/classes/Booking'
+import testBookings from './test-data/test-bookings'
+import testRooms from './test-data/test-rooms'
+import testUsers from './test-data/test-users'
+
 
 
 describe('Hotel', () => {
-  let hotel;
+  let hotel, room, booking;
 
   beforeEach() => {
-      hotel = new Hotel();
+
+    hotel = new Hotel(testRooms, testBookings);
   }
+
+  it.skip('should be a function', () => {
+    expect(Hotel).to.be.a('function');
+  });
+
+  it.skip('should instantiate an instance of Hotel', () => {
+    expect(hotel).to.be.an.instanceof('Hotel');
+  });
+
+  it.skip('should store all rooms data', () => {
+    expect(hotel.rooms).to.deep.equal(testRooms);
+  });
+
+  it.skip('should store all booking data', () => {
+    expect(hotel.bookings).to.deep.equal(testBookings);
+  });
+
+  it.skip('should have a way to store all available rooms', () => {
+    expect(hotel.availableRooms).to.deep.equal([]);
+  });
+
+
+
 
 })
