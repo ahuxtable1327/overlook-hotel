320590a9bdc1454ba34a9670ed953a7afc65232a
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-12 09:27:58 -0400

    add tests to check for class properties

--- test/Room-test.js
+++ test/Room-test.js
@@ -1,14 +1,16 @@
 import chai from 'chai';
 const expect = chai.expect;
-import testRooms from './test/test-rooms'
+import testRooms from './test/test-rooms';
 
 import Room from './src/classes/Room';
 
 
 describe('Room', () => {
+`   `let room, room2;
 
   beforeEach() => {
-
+    room = new Room(testRooms[0]);
+    room2 = new Room(testRooms[1]);
   }
 
   it.skip('should be a function', () => {
@@ -20,28 +22,33 @@ describe('Room', () => {
   })
 
   it.skip('should have a room number', () => {
-    expect(room.roomNumber).to.equal(1);
+    expect(room).to.have.a.property('number');
+    expect(room.number).to.equal(1);
   });
 
   it.skip('should have a room type', () => {
+    expect(room).to.have.a.property('roomType');
     expect(room.roomType).to.equal('residential suite');
   });
 
   it.skip('should state whether it has a bidet or not', () => {
-    expect(room.hasBidet).to.equal(true);
-    expect(room2.hasBidet).to.equal(false);
+    expect(room).to.have.a.property('bidet');
+    expect(room.bidet).to.equal(true);
+    expect(room2.bidet).to.equal(false);
   });
 
   it.skip('should have a bed size', () => {
+    expect(room).to.have.a.property('bedSize');
     expect(room.bedSize).to.equal('queen');
   })
 
   it.skip('should tell the number of beds', () => {
+    expect(room).to.have.a.property('numBeds');
     expect(room.numBeds).to.equal(1);
   });
 
   it.skip('should have a cost per night', () => {
+    expect(room).to.have.a.property('costPerNight');
     expect(room.costPerNight).to.equal(358.4);
   });
-
 })
