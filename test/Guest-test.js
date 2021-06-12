338ac5837322a3e6ac3769dd9fe36b0a82816fa3
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-12 11:20:54 -0400

    Pass all tests for Guest class

--- test/Guest-test.js
+++ test/Guest-test.js
@@ -11,25 +11,25 @@ describe.only('Guest', () => {
     guest = new Guest(testUsers[0])
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Guest).to.be.a('function');
   });
 
-  it.skip('should be an instance of Guest', () => {
+  it('should be an instance of Guest', () => {
     expect(guest).to.be.an.instanceof(Guest);
   });
 
-  it.skip('should have an id', () => {
+  it('should have an id', () => {
     expect(guest).to.have.a.property('id');
     expect(guest.id).to.equal(1);
   });
 
-  it.skip('should have a name', () => {
+  it('should have a name', () => {
     expect(guest).to.have.a.property('name');
     expect(guest.name).to.equal('Leatha Ullrich');
   });
 
-  it.skip('should have a way to store all of a guests bookings', () => {
+  it('should have a way to store all of a guests bookings', () => {
     expect(guest).to.have.a.property('bookings');
     expect(guest.bookings).to.deep.equal([]);
   })
