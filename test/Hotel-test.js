6dc661393c91bf1167dfe91baffebfaee42cba4d
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-12 10:01:09 -0400

    Add imports and describe block

---
+++ test/Hotel-test.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Hotel from './src/classes/Hotel'
+
+
+describe('Hotel', () => {
+  let hotel;
+
+  beforeEach() => {
+      hotel = new Hotel();
+  }
+
+})
