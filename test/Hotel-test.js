5d42b4fa7c106ec5a7dea9e858437329d7481195
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-12 10:51:21 -0400

    Add tests to check for booking data and available rooms

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -10,10 +10,13 @@ import testUsers from './test-data/test-users'
 
 
 describe('Hotel', () => {
-  let hotel, room, booking;
+  let hotel, room1, room2, room3, booking, guest;
 
   beforeEach() => {
-
+    room1 = testRooms[0];
+    room2 = testRooms[1];
+    room3 = testRooms[2];
+    guest = new Guest(testUsers[0])
     hotel = new Hotel(testRooms, testBookings);
   }
 
@@ -37,7 +40,46 @@ describe('Hotel', () => {
     expect(hotel.availableRooms).to.deep.equal([]);
   });
 
+  it.skip('should display all of a guests bookings', () => {
+    let guestBookings = hotel.getGuestBookings(guest);
+    let bookings = [
+      {
+        "id": "5fwrgu4i7k55hl6sz",
+        "userID": 1,
+        "date": "2020/04/22",
+        "roomNumber": 1,
+        "roomServiceCharges": []
+      },
+      {
+        "id": "5fwrgu4i7k55hl6t8",
+        "userID": 1,
+        "date": "2020/02/05",
+        "roomNumber": 3,
+        "roomServiceCharges": []
+      },
+    ]
+    expect(guestBookings).to.deep.equal(bookings);
+  });
 
+  it.skip('should be able to calculate a users total amount spent at the hotel', () => {
+    let guestTotal = hotel.getGuestAmntSpent(user);
+    expect(guestTotal).to.equal(849.54);
+  });
 
+  it.skip('should get room information for each booking', () => {
+    let booking =  testBookings[0];
+    let room = hotel.getRoomInformation(booking)
+    expect(booking.roomNumber).to.equal(room.number);
+    expect(room).to.equal(testRooms[0])
+  })
 
+  it.skip('should be able to find available rooms for specific dates', () => {
+    let availableRooms = hotel.getAvailableRooms('2020/04/22');
+    expect(availableRooms).to.deep.equal([room2, room3])
+  })
+
+  it.skip('should not get any rooms if they are all booked on a date', () => {
+    let noRoom = hotel.getAvailableRooms('2020/01/24');
+    expect(noRoom).to.equal('Sorry, there are no rooms available for this date!');
+  })
 })
