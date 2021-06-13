5a1c672d6fd44a10815dfe3bde70725ceb289663
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-13 15:40:49 -0500

    Add function to show user bookings and update fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,47 @@
-const fetchData = (type) => {
-  return fetch(`http://localhost:3001/api/v1/${type}`)
+const fetchGuestData = () => {
+  return fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json())
-    .catch(err => console.error('err'))
+    .catch(err => console.error('Something went wrong with customers'))
 }
 
+const fetchRoomData = () => {
+  return fetch('http://localhost:3001/api/v1/rooms')
+    .then(response => response.json())
+    .catch(err => console.error('Something went wrong with rooms'))
+}
+
+const fetchBookingData = () => {
+  return fetch('http://localhost:3001/api/v1/bookings')
+    .then(response => response.json())
+    .catch(err => console.error('Something went wrong with bookings'))
+}
+
+const fetchUser = (id) => {
+  return fetch(`http://localhost:3001/api/v1/customers/${id}`)
+    .then(response => response.json())
+    .catch(err => console.error('Something went wrong with user'))
+}
+
+const getData = () => Promise.all([fetchGuestData(), fetchRoomData(), fetchBookingData()]);
 
 
-const postData = (booking) => {
-  return fetch(`http://localhost:3001/api/v1/${booking}`, {
+
+const postData = () => {
+  return fetch(`http://localhost:3001/api/v1/bookings`, {
     method: 'POST',
     body: JSON.stringify(booking),
     headers: {
       'Content-type': 'application/json'
     }
   })
+  .then(checkForError)
 }
 
-export default fetchData;
-export default postData;
+const checkForError = (response) => {
+  if(!response.ok) {
+    // dom update to show error???
+    throw new Error('Something went wrong')
+  } else {
+    return response.json();
+  }
+}
--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,34 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-import apiCalls from './apiCalls'
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './images/turing-logo.png'
+import Hotel from './classes/Hotel'
+import Guest from './classes/Guest'
+import Booking from './classes/Booking'
+import Room from './classes/Room'
+import fetchData from './apiCalls'
+
+let dayjs = require('dayjs');
+
+let guest, room, booking;
+
+window.addEventListener('load', getData);
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+function getData(){
+  // fetchData('customer')
+  //   .then(guest => {
+  //     guest = new Guest(guest[0])
+  //   });
+  // fetchData('rooms')
+  //   .then(room => {
+  //     room = new Room(room)
+  //   });
+  // fetchData('bookings')
+  //   .then(booking => {
+  //     booking = new Booking(booking)
+  //   });
+}
+
+  console.log('This is the JavaScript entry file - your code begins here.');
