3aaa5c09e16f45517beae9220513a62e53309e0a
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-13 09:22:34 -0400

    Add methods to check for guest bookings and room information

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -5,6 +5,18 @@ class Hotel {
     this.availableRooms = [];
   }
 
+  getGuestBookings(guest) {
+    let bookings = this.bookings.filter(booking => booking.userID === guest.id);
+    guest.bookings = bookings;
+    return bookings;
+  }
+
+  getRoomInformation(booking) {
+    return this.rooms.find(room => room.number === booking.roomNumber)
+  }
+
+  
+
 
 }
 
